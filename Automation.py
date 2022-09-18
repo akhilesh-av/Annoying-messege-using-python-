f9430d0e14373412a044e5dec3b2c3b589824f52
Akhilesh A V <68072131+noobie8@users.noreply.github.com>
2022-09-18 16:51:54 +0530

    Automation messege using python

---
+++ Automation.py
@@ -0,0 +1,18 @@
+"""
+This is code for anoying anyone or automate the messege at a number of times in a digital space
+This code will work when you run the code on an ide then where you click "Enter" , (Triger of the code is Enter Button in keyboard)
+"""
+
+
+
+import pyautogui as pg
+import time
+
+print("run in 5 Sec")               # giving intication to user
+time.sleep(5)
+
+
+for i in range(100):            #code will run 100 time , means on click of "Enter" it send 100 time . 
+    pg.write("What ever you want to write for 100 time ")
+    time.sleep(0.5)
+    pg.press("Enter")    #OnClick of "Enter" button message send 100 time on where you click  

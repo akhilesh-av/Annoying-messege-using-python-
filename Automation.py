3a4f898cf3c5c559e534784236405cd83697ced1
akhilesh <akhileshav8@gmail.com>
2025-01-06 16:30:08 +0530

    deleted unwanted lines

--- Automation.py
+++ Automation.py
@@ -6,8 +6,7 @@ This code will work when you run the code on an ide then where you click "Enter"
 import pyautogui as pg
 import time
 
-print("This code will run when you click Enter Button in keyboard hasdj")  # giving intication to user
-print('hello')
+
 print("run in 5 Sec ")   # giving intication to user
 time.sleep(5)
 
